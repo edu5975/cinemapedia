3dc5e44429ad6bdb4c0bbccf5ce40917c78f000d
Your Name <you@example.com>
2023-06-07 13:19:26 -0600

    movie db mapper y repository

--- lib/infrastructure/datasources/themoviedb_datasource.dart
+++ lib/infrastructure/datasources/themoviedb_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 class ThemoviedbDatasource extends MoviesDatasource {
@@ -14,7 +16,12 @@ class ThemoviedbDatasource extends MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
 
-    final List<Movie> movies = [];
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDbResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDbToEntity(moviedb))
+        .toList();
 
     return movies;
   }
